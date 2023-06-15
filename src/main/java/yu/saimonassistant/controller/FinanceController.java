66f1127adef692d063b3d571ce33628045aef9c2
NBDatsuya <2646809050@qq.com>
2023-06-16 00:45:42 +0800

    feat: FinanceController

--- src/main/java/yu/saimonassistant/controller/FinanceController.java
+++ src/main/java/yu/saimonassistant/controller/FinanceController.java
@@ -11,6 +11,8 @@ import yu.saimonassistant.dto.HttpResponse;
 import yu.saimonassistant.entity.Finance;
 import yu.saimonassistant.service.FinanceService;
 
+import java.util.List;
+
 @RestController
 @RequestMapping(value = "/api/finance")
 public class FinanceController {
@@ -28,6 +30,75 @@ public class FinanceController {
 				return new HttpResponse(1, result, "删除成功");
 		} catch (Exception e) {
 			return new HttpResponse(0, result, "请求错误");
+		}
+	}
+
+	@RequestMapping(value = "/modify", method = RequestMethod.POST)
+	public HttpResponse modifyFinance(@RequestBody Finance finance) {
+		int result = -1;
+		result = service.modifyFinanceInfo(finance);
+		try {
+			if (result == 0)
+				return new HttpResponse(0, result, "修改失败");
+			else
+				return new HttpResponse(1, result, "修改成功");
+		} catch (Exception e) {
+			e.printStackTrace();
+			return new HttpResponse(0, result, "请求错误");
+		}
+	}
+
+	@RequestMapping(value = "/queryList", method = RequestMethod.POST)
+	public HttpResponse queryFinanceList() {
+		try {
+			List<Finance> result = service.queryFinanceList();
+			if (result.isEmpty())
+				return new HttpResponse(1, result, "收支记录为空");
+			else
+				return new HttpResponse(1, result, "查询成功");
+		} catch (Exception e) {
+			e.printStackTrace();
+			return new HttpResponse(0, null, "请求错误");
+		}
+	}
+
+	@RequestMapping(value = "/query", method = RequestMethod.POST)
+	public HttpResponse queryFinance(@RequestBody Finance finance) {
+		try {
+			Finance result = service.selectById(finance);
+			if (result == null)
+				return new HttpResponse(0, null, "不存在");
+			else
+				return new HttpResponse(1, result, "查询成功");
+		} catch (Exception e) {
+			e.printStackTrace();
+			return new HttpResponse(0, null, "请求错误");
+		}
+	}
+
+	@RequestMapping(value = "/stat", method = RequestMethod.POST)
+	public HttpResponse queryStat(@RequestBody FinanceRangeRequest req) {
+		try {
+			FinanceStatResponse result = service.queryStat(req);
+			return new HttpResponse(1, result, "查询成功");
+		} catch (Exception e) {
+			e.printStackTrace();
+			return new HttpResponse(0, null, "请求错误");
+		}
+	}
+
+	@RequestMapping(value = "/add", method = RequestMethod.POST)
+	public HttpResponse addFinanceInfo(@RequestBody Finance finance){
+		int result = -1;
+		try {
+			result= service.addFinanceInfo(finance);
+			if (result == 0)
+				return new HttpResponse(0,result,"添加失败");
+			else
+				return new HttpResponse(1,result,"添加成功");
+
+		} catch (Exception e) {
+			return new HttpResponse(0,result,"请求错误");
 		}
 	}
 }
