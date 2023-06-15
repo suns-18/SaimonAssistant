43b7379641fbeff2c9ebbdb83435a34c8caa2b1c
NBDatsuya <2646809050@qq.com>
2023-06-15 14:30:48 +0800

    init commit

---
+++ src/test/java/yu/saimonassistant/SaimonAssistantApplicationTests.java
@@ -0,0 +1,13 @@
+package yu.saimonassistant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaimonAssistantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
