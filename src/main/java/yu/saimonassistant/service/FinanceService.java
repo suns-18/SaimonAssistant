0994268cf8c0f3a32af7803e7e4baae35084deb7
NBDatsuya <2646809050@qq.com>
2023-06-16 00:45:37 +0800

    feat: FinanceService

--- src/main/java/yu/saimonassistant/service/FinanceService.java
+++ src/main/java/yu/saimonassistant/service/FinanceService.java
@@ -8,6 +8,9 @@ import yu.saimonassistant.entity.Finance;
 import yu.saimonassistant.mapper.FinanceMapper;
 import yu.saimonassistant.util.UUIDUtil;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 public class FinanceService {
 	@Autowired
@@ -33,5 +36,23 @@ public class FinanceService {
 
 	public int deleteById(Finance finance) {
 		return mapper.deleteByPrimaryKey(finance.getId());
+	}
+
+	public int modifyFinanceInfo(Finance finance){ return mapper.updateByPrimaryKey(finance); }
+
+	public List<Finance> queryFinanceList(){
+		// 你这里获取的是列表，不是单个
+		// 参数没有意义，别写
+		return mapper.selectAll();
+	}
+
+	public Finance selectById(Finance finance){
+		// 你这里获取的是单个，不是列表
+		return mapper.selectByPrimaryKey(finance.getId());
+	}
+	public int addFinanceInfo(Finance finance){
+		// 插入前生成ID
+		finance.setId(UUIDUtil.getOneUUID());
+		return mapper.insert(finance);
 	}
 }
