b48c786f3dc6af2b581cc0fce3e5acb37092654b
wangwenqingqq <1064078316@qq.com>
2023-06-26 08:50:35 +0800

    fix:HealthTest

--- src/test/java/yu/seimonassistant/HealthTest.java
+++ src/test/java/yu/seimonassistant/HealthTest.java
@@ -21,47 +21,68 @@ class HealthTest {
 
 		Assertions.assertNotNull(c.queryList().getData(),
 			"返回列表出错");
-		log.info("query功能>>测试1：查询所有健康信息，返回所有，通过");
+		log.info("query功能>>测试1：查询所有健康信息，返回所有，测试通过");
 		var h = new Health();
 		h.setId(UUIDUtil.getOneUUID());
 		Assertions.assertNull(c.queryById(h).getData(),
 			"查找一个不存在的健康信息记录");
-		log.info("query功能>>测试2：查询不存在的单一健康信息，返回为空，通过");
+		log.info("query功能>>测试2：查询不存在的单一健康信息，返回为空，测试通过");
 		h.setId("0127d8a43700dfa784ef5ba41bd35500");
 		Assertions.assertNotNull(c.queryById(h).getData(),
 			"查找一个存在的健康信息记录");
-		log.info("query功能>>测试2：查询存在的单一健康信息，返回正常，通过");
+		log.info("query功能>>测试2：查询存在的单一健康信息，返回正常，测试通过");
 		h.setId(null);
 		c.queryById(h);
 		Assertions.assertNull(c.queryById(h).getData(),
 			"查找健康信息记录参数错误");
-		log.info("query功能>>测试3：查询id为null的单一健康信息，返回为空，通过");
-		log.info("query功能>>单个请求测试，通过");
+		log.info("query功能>>测试3：查询id为null的单一健康信息，返回为空，测试通过");
+		log.info("query功能>>单个请求测试通过");
 	}
 
 	@Test
 	void add() {
 		Assertions.assertEquals(c.add(null).getCode(), 0);
-
+		log.info("add功能>>测试1：添加一条null的健康信息，无法添加，测试通过");
 
 		var h = new Health();
 		h.setTitle("aaa");
 		Assertions.assertEquals(c.add(h).getCode(), 1);
+		log.info("add功能>>测试2：添加一条健康信息，添加成功，测试通过");
+		h.setTitle("");
+		Assertions.assertEquals(c.add(h).getCode(), 0);
+		log.info("add功能>>测试3：添加一条标题为空串健康信息，无法添加，测试通过");
+		log.info("add功能>>单个请求测试通过");
 	}
 
 	@Test
 	void update() {
 		Assertions.assertEquals(c.modify(null).getData(), 0);
-
+		log.info("update功能>>测试1：更新一条null的健康信息，无法更新，测试通过");
 		var h = new Health();
 		h.setId("0127d8a43700dfa784ef5ba41bd35500");
 		h.setTitle("bbb");
 		Assertions.assertEquals(c.modify(h).getCode(), 1);
-
+		log.info("update功能>>测试2：更新一条正常的健康信息，更新成功，测试通过");
 		h.setId("114514");
 		h.setTitle("bbb");
 		Assertions.assertEquals(c.modify(h).getCode(), 0);
+		log.info("update功能>>测试3：更新一条id不存在的健康信息，无法更新，测试通过");
+		log.info("update功能>>单个请求测试通过");
 	}
-
+	@Test
+	void del() {
+		Assertions.assertEquals(c.delete(null).getData(), 0);
+		log.info("delete功能>>测试1：删除一条null的健康信息，无法删除，测试通过");
+		var h = new Health();
+		h.setId("0127d8a43700dfa784ef5ba41bd35500");
+		h.setTitle("bbb");
+		Assertions.assertEquals(c.modify(h).getCode(), 1);
+		log.info("delete功能>>测试2：删除一条正常的健康信息，更新成功，测试通过");
+		h.setId("114514");
+		h.setTitle("bbb");
+		Assertions.assertEquals(c.modify(h).getCode(), 0);
+		log.info("delete功能>>测试3：删除一条id不存在的健康信息，无法更新，测试通过");
+		log.info("delete功能>>单个请求测试通过");
+	}
 
 }
