b54242c98a5066773d36d4edb2db99f751daa057
wangwenqingqq <1064078316@qq.com>
2023-06-26 10:00:51 +0800

    fix:SeimonAssistantApplication

--- src/main/java/yu/seimonassistant/SeimonAssistantApplication.java
+++ src/main/java/yu/seimonassistant/SeimonAssistantApplication.java
@@ -1,8 +1,9 @@
 package yu.seimonassistant;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+@MapperScan
 @SpringBootApplication
 public class SeimonAssistantApplication {
 	public static void main(String[] args) {
